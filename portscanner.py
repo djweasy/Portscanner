9001d6994e4d3758f6d1ea7baf3b2b752f651039
djweasy <82163676+djweasy@users.noreply.github.com>
2022-02-13 07:22:40 +0100

    Portscaanner

---
+++ portscanner.py
@@ -0,0 +1,23 @@
+import socket
+import subprocess
+import sys
+from datetime import date, datetime
+
+target = "192.168.178.23"
+targetIP = socket.gethostbyname(target)
+tstart = datetime.now()
+
+try:
+    for p in range(1, 45000):
+        sock =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        res = sock.connect_ex((targetIP, p))
+        if res == 0:
+            print("Offene Verbindung in Port " + str(p))
+        sock.close()
+except Exception:
+    print("There was an error.")
+    sys.exit()
+tend = datetime.now()
+diff = tend - tstart
+
+print("Scan completed in " + str(diff))
